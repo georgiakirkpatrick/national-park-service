e19e97241cf3c28b31421ff568d4b5c5c1b7cb1b
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2019-11-21 13:57:05 -0800

    cleaned up console.logs

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ let apiKey = '4VMtIA1dYHaYcs7tcmYnHcABNSez3QKNCuCnLOmz'
 function displayParks(responseJson) {
     $('.js-results').empty()
     $('.js-results').append(`<h2>Results</h2>`)
-    console.log('`displayParks` ran')
-    console.log(responseJson)
+    console.log('`displayParks` ran.')
+    console.log('responseJson:', responseJson)
 
     for (let i=0; i < responseJson.data.length; i++) {
-        console.log(responseJson.data[i].addresses[0].line1)
         $('.js-results').append(`<h2>${responseJson.data[i].fullName}</h2>
         <p>${responseJson.data[i].description}</p><br>
         <p>Park page: <a href=${responseJson.data[i].url}>${responseJson.data[i].url}</a></p><br>
         <p>Address:</p>`)
 
-        for (let j=0; j < responseJson.data[i].addresses.length; j++) {
+        for (let j=0; j < (responseJson.data[i].addresses.length - 1); j++) {
             if (responseJson.data[i].addresses[j].type === "Physical") {
                 $('.js-results').append(`<p>${responseJson.data[i].addresses[j].line1}</p>
                 <p>${responseJson.data[i].addresses[j].line2}</p>
@@ -26,17 +25,15 @@ function displayParks(responseJson) {
                 ${responseJson.data[i].addresses[j].stateCode}, 
                 ${responseJson.data[i].addresses[j].postalCode}</p>`)
             }
-        }    
-            
+        }     
     }
 }
 
 function getNationalParks(stateCodes, requestedNumberResults) {
-    console.log(`getNationalParks ran, stateCodes is ${stateCodes}, and requestedNumberResults ${requestedNumberResults}`)
+    console.log('`getNationalParks` ran, `stateCodes` is ', stateCodes, ' and `requestedNumberResults` is ', requestedNumberResults)
     fetch (`https://developer.nps.gov/api/v1/parks?stateCode=${stateCodes}&limit=${requestedNumberResults}&api_key=${apiKey}&fields=addresses`)
         .then(response => response.json())
         .then(responseJson => 
-            // console.log(responseJson))
             displayParks(responseJson))
         .catch(error => {
             console.log("There is an error.")
@@ -44,22 +41,16 @@ function getNationalParks(stateCodes, requestedNumberResults) {
 }
 
 function formatStates(selectedStates, requestedNumberResults) {
-    console.log(`selectedStates is ${selectedStates} and selectedStates.length is ${selectedStates.length}`)
-
     let stateCodes = ``
 
     if (selectedStates.length === 1) {
         stateCodes = selectedStates
-        console.log(`stateCodes is ${stateCodes}`)
-    }
+        }
 
     else {
-        console.log('formatStates `else` statement ran')
         stateCodes = selectedStates[0]
         for (let i = 1; i < selectedStates.length; i++) {
-            console.log('formatStates `for` statement ran')
             stateCodes = stateCodes + `&stateCode=${selectedStates[i]}`
-            console.log(`stateCodes is ${stateCodes}`)
         }
     }
 
